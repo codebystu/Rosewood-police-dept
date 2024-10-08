0320bbc6782d7756585925b2e3e5b30fb532ebc6
codebystu <stubet17@outlook.com>
2024-10-08 16:43:38 +0100

    add event listeners but input box not picking up inner text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-var passWord = "Art2822"
+var validlog = "Art2822"
 
 function checkPassword() {
-
+    let logattempt = document.getElementById("login-box").innerText;
+        console.log(logattempt)
+        console.log(validlog)  
+    
 }
+
+let logbut = document.getElementById("pass-sub");
+    logbut.addEventListener('click', checkPassword);
 
 function closeLogin() {
     document.getElementById("login-screen").style.display ="none";
 }
+
+// function whiteScreen() {
+//     document.getElementById("login-screen").style.backgroundColor = "white";
+// }
+
+// whiteScreen()
